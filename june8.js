090da97c1d0b1078266c6fc07ab2bf3eb8f3576d
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-07-06 15:35:21 -0600

    added june10

--- june8.js
+++ june8.js
@@ -1,9 +1,10 @@
 var fish = [ "rainbow trout", "smallmouth bass", "white bass", "channel catfish" ]
 
 var capitalizeFish = function (arr) {
-  arr.forEach(function(list){
-    console.log(list.charAt(0).toUpperCase() + list.slice(1));
-  })
+
+  // arr.forEach(function(list){
+  //   console.log(list.charAt(0).toUpperCase() + list.slice(1));
+  // })
 }
 
 console.log(capitalizeFish(fish))
