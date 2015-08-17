9f06f477d0678842514f0600d6abafee17883f0b
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-08-17 15:59:17 -0600

    working on Aug17

--- august17.js
+++ august17.js
@@ -1,14 +1,37 @@
 // Create a new instance of the object Yourself.
 // You woke up this morning tired, hungry, and at home.
 // Write at least three functions that change
-// the state of the instance of yourself to how you feel and where you're at now.
+// the state of the instance of yourself to how you feel
+// and where you're at now.
 
-var Kim = function () {
+var Student = function (name) {
+  this.name = name
   this.energy = 0
+  this.hungry = true
+  this.home = true
 }
 
-Kim.prototype.sleep = function (hours) {
+Student.prototype.sleep = function (hours) {
   this.energy += hours
 }
 
-Kim.prototype.appetite = function ()
+Student.prototype.eat = function (food) {
+  this.hungry = false
+  this.energy += food/2
+}
+
+Student.prototype.travel = function (location) {
+  if (location === 'home') {
+    this.home = true
+  }
+  else {
+    this.home = false
+  }
+}
+
+var kim = new Student('Kim')
+console.log(kim.home + ' before');
+kim.travel('galvanize')
+console.log(kim.home + ' after');
+kim.travel('home')
+console.log(kim.home + ' after school');
