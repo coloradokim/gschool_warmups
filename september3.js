11dceb57e930f385e8ca0713766a031a6c5b5827
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-09-04 10:17:42 -0600

    solution for september 3

--- september3.js
+++ september3.js
@@ -4,13 +4,38 @@
 // Check if it's a palindrome (in this case 55 is one, so we'd stop)
 // If not, repeat steps 1 - 3 until it is.
 
+
+
+
+
 var findLych = function (num) {
   num2 = parseInt((num + '').split('').reverse().join(''))
   checkNum = num + num2
-  if (checkNum === palindrome){
+  if (checkNum === palindrome){ //how do I check for a palindrome?
     return checkNum
   } else {
-    repeat findLync with checkNum
+    repeat findLync() with checkNum // how do I do this? this is recursion
   }
 }
 console.log(findLych(31));
+
+// Jenny's Solution
+// num = parseInt(Math.random() * 1000)
+// // or try 187! don’t try 196!
+// //num = 187
+// startNum = num
+// count = 0
+// console.log(num)
+// revNum = parseInt(num.toString().split('').reverse().join(''))
+// console.log(revNum)
+//
+// while (num !== revNum){
+//     num = num + revNum
+//     revNum = parseInt(num.toString().split('').reverse().join(''))
+//     console.log(num)
+//     count += 1
+// }
+//
+// console.log('ending number', num)
+// console.log('starting number', startNum)
+// console.log('count', count)
